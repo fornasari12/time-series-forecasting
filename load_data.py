4d834b997549c3dc8d2bd4f86edd46ed62713d77
Nicolas Fornasari <fornasari12@gmail.com>
2021-11-14 20:44:22 -0300

    Added benchmark data, and LoadData class to handle multiple .csv and changes in train pipleline

---
+++ load_data.py
@@ -0,0 +1,133 @@
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+
+
+class LoadData:
+    """
+    This Class will handle and load CSV files into the
+    PyTorch TimeSeriesDataSet
+
+    """
+    def __init__(
+            self,
+            data_path: str,
+            folder_list: str,
+            cutoff: float
+    ):
+        self.data_path = data_path
+        self.folder_list = folder_list
+        self.cutoff = cutoff
+        self.train_data = pd.DataFrame()
+        self.test_data = pd.DataFrame()
+
+    def get_file_list(self, folder: str):
+
+        folder_path = f"{self.data_path}/{folder}"
+        file_list = [
+            f for f in listdir(folder_path) if isfile(join(folder_path, f))
+        ]
+
+        return file_list
+
+    def _load_dataframe(
+            self,
+            folder: str,
+            file: str
+    ):
+
+        file_path = f"{self.data_path}/{folder}/{file}"
+        df = pd.read_csv(file_path)
+
+        # FIXME: Auto detect date column
+        df = df.rename(columns={"timestamp": "date"})
+
+        return df
+
+    @staticmethod
+    def _create_id_columns(
+            df: pd.DataFrame,
+            folder: str,
+            file: str):
+
+        df["dataset"] = folder
+        df["id"] = file.replace(".csv", "")
+
+    @staticmethod
+    def _create_date_features(df: pd.DataFrame):
+
+        df["date"] = pd.to_datetime(df["date"])
+        df["month"] = pd.to_datetime(df.date).dt.month \
+            .astype(str) \
+            .astype("category")
+        df["day_of_week"] = pd.to_datetime(df.date).dt.dayofweek \
+            .astype(str) \
+            .astype("category")
+        df["hour"] = pd.to_datetime(df.date).dt.hour \
+            .astype(str) \
+            .astype("category")
+
+    @staticmethod
+    def _create_time_idx(df: pd.DataFrame):
+        df["time_idx"] = list(range(len(df)))
+
+    @staticmethod
+    def _train_test_split(
+            df,
+            train_split
+    ):
+        df_train = df.iloc[:train_split, :]
+        df_test = df.iloc[train_split:, :]
+
+        print(len(df_train), len(df_test))
+
+        return df_train, df_test
+
+    def _concatenate_df(
+            self,
+            df_train: pd.DataFrame,
+            df_test: pd.DataFrame
+    ):
+        self.train_data = pd.concat(
+            objs=[self.train_data, df_train],
+            axis=0
+        )
+        self.test_data = pd.concat(
+            objs=[self.test_data, df_test],
+            axis=0
+        )
+
+        self.train_data = self.train_data.reset_index(drop=True)
+        self.test_data = self.test_data.reset_index(drop=True)
+
+    def load_data(self):
+
+        for folder in self.folder_list:
+
+            file_list = self.get_file_list(
+                folder=folder
+            )
+
+            for file in file_list:
+                df = self._load_dataframe(folder=folder, file=file)
+                train_split = int(len(df) * self.cutoff)
+
+                self._create_id_columns(
+                    df=df,
+                    folder=folder,
+                    file=file
+                )
+                self._create_date_features(df=df)
+                self._create_time_idx(df=df)
+
+                df_train, df_test = self._train_test_split(
+                    df=df,
+                    train_split=train_split
+                )
+
+                self._concatenate_df(
+                    df_train=df_train,
+                    df_test=df_test
+                )
+
+        return self.train_data, self.test_data
---
+++ tft_pipeline_colab.py
@@ -0,0 +1,139 @@
+import warnings
+
+from config import load_config
+from load_data import LoadData
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+from pytorch_lightning.loggers import TensorBoardLogger
+import torch
+
+from pytorch_forecasting import Baseline, TemporalFusionTransformer, TimeSeriesDataSet
+from pytorch_forecasting.data import GroupNormalizer
+from pytorch_forecasting.metrics import QuantileLoss
+
+import tensorflow as tf
+import tensorboard as tb
+tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
+
+warnings.filterwarnings("ignore")
+
+spec = load_config("/content/temporal-fusion-transformer/config.yaml")
+DATA_PATH = spec["general"]["data_path"]
+FOLDER_LIST = spec["general"]["folder_list"]
+BATCH_SIZE = spec["model"]["batch_size"]
+MAX_EPOCHS = spec["model"]["max_epochs"]
+GPUS = spec["model"]["gpus"]
+LEARNING_RATE = spec["model"]["learning_rate"]
+HIDDEN_SIZE = spec["model"]["hidden_size"]
+DROPOUT = spec["model"]["dropout"]
+HIDDEN_CONTINUOUS_SIZE = spec["model"]["hidden_continuous_size"]
+GRADIENT_CLIP_VAL = spec["model"]["gradient_clip_val"]
+
+if __name__ == "__main__":
+
+    train_data, _ = LoadData(
+        data_path="/content/temporal-fusion-transformer/" + DATA_PATH,
+        folder_list=FOLDER_LIST,
+        cutoff=0.70
+    ).load_data()
+
+    max_prediction_length = 24
+    max_encoder_length = 48
+    # training_cutoff = data["time_idx"].max() - max_prediction_length
+
+    training = TimeSeriesDataSet(
+        train_data,
+        time_idx="time_idx",
+        target="value",
+        group_ids=["dataset", "id"],
+        min_encoder_length=max_encoder_length // 2,
+        max_encoder_length=max_encoder_length,
+        min_prediction_length=1,
+        max_prediction_length=max_prediction_length,
+        static_categoricals=["id"],
+        time_varying_known_reals=["time_idx"],
+        time_varying_known_categoricals=["hour", "month", "day_of_week"],
+        time_varying_unknown_categoricals=[],
+        time_varying_unknown_reals=["value"],
+        target_normalizer=GroupNormalizer(
+            groups=["dataset", "id"], transformation="softplus"
+        ),
+        add_relative_time_idx=True,
+        add_target_scales=True,
+        add_encoder_length=True,
+
+    )
+
+    # create validation set (predict=True) which means to predict the
+    # last max_prediction_length points in time for each series
+    validation = TimeSeriesDataSet.from_dataset(
+        training,
+        train_data,
+        predict=True,
+        stop_randomization=True
+    )
+
+    # create dataloaders for model
+    train_dataloader = training.to_dataloader(
+        train=True,
+        batch_size=BATCH_SIZE,
+        num_workers=0)
+    val_dataloader = validation.to_dataloader(
+        train=False,
+        batch_size=BATCH_SIZE * 10,
+        num_workers=0
+    )
+
+    # calculate baseline mean absolute error, i.e. predict next value as the
+    # last available value from the history
+    actuals = torch.cat([y for x, (y, weight) in iter(val_dataloader)])
+    baseline_predictions = Baseline().predict(val_dataloader)
+    (actuals - baseline_predictions).abs().mean().item()
+
+    # configure network and trainer
+    pl.seed_everything(42)
+
+    # configure network and trainer
+    early_stop_callback = EarlyStopping(
+        monitor="val_loss",
+        min_delta=1e-4,
+        patience=10,
+        verbose=False,
+        mode="min"
+    )
+    lr_logger = LearningRateMonitor()
+    logger = TensorBoardLogger("lightning_logs")
+
+    trainer = pl.Trainer(
+        max_epochs=MAX_EPOCHS,
+        gpus=GPUS,
+        weights_summary="top",
+        gradient_clip_val=GRADIENT_CLIP_VAL,
+        # limit_train_batches=30,
+        # fast_dev_run=True,
+        callbacks=[lr_logger, early_stop_callback],
+        logger=logger,
+    )
+
+    tft = TemporalFusionTransformer.from_dataset(
+        training,
+        learning_rate=LEARNING_RATE,
+        hidden_size=HIDDEN_SIZE,
+        attention_head_size=1,
+        dropout=DROPOUT,
+        hidden_continuous_size=HIDDEN_CONTINUOUS_SIZE,
+        output_size=7,  # 7 quantiles by default
+        loss=QuantileLoss(),
+        log_interval=10,
+        reduce_on_plateau_patience=4,
+    )
+
+    # fit network
+    trainer.fit(
+        tft,
+        train_dataloader=train_dataloader,
+        val_dataloaders=val_dataloader,
+    )
+
+    torch.save(tft.state_dict(), "/content/model/tft_regressor_test.pt")
